2c566165ed9e40750889b01ae171e0b2520bed94
Oscar2Javea <oscardvc1@hotmail.com>
2026-05-29 15:54:43 +0200

    Allow fullstack frontend

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://react-task-manager-phi-two.vercel.app'
+    'https://react-task-manager-phi-two.vercel.app',
+    'https://fullstack-task-manager-puce.vercel.app'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type']
